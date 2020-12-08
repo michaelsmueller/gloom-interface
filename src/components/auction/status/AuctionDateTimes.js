f881129c8e99cbaa6d7240d95bbee233eaa3a99f
Michael Mueller <hello@michaelmueller.dev>
2020-12-08 16:26:31 +0100

    show auction time left in AuctionDetail

---
+++ src/components/auction/status/AuctionDateTimes.js
@@ -0,0 +1,37 @@
+/* eslint-disable no-console */
+import React, { useEffect, useState } from 'react';
+import { calculateTimeLeft, showLocalDateTime } from 'utils/dateTime';
+
+export default function AuctionDateTimes({ auctionDateTimes }) {
+  const { startDateTime, endDateTime } = auctionDateTimes;
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft(endDateTime));
+
+  useEffect(() => {
+    const timer = setTimeout(() => setTimeLeft(calculateTimeLeft(endDateTime)), 1000);
+    return () => clearTimeout(timer);
+  });
+
+  const timerComponents = [];
+  Object.keys(timeLeft).forEach(interval => {
+    if (!timeLeft[interval]) return;
+    timerComponents.push(
+      <span key={interval}>
+        {timeLeft[interval]} {interval}{' '}
+      </span>,
+    );
+  });
+
+  // console.log(`timeLeft, ${JSON.stringify(timeLeft, null, 2)}`);
+  return (
+    <pre>
+      <h2>Auction dates & times</h2>
+      <ul>
+        {/* <li>Now: {showLocalDateTime(now)}</li> */}
+        <li>Start date & time:&nbsp; {showLocalDateTime(startDateTime)}</li>
+        <li>End date & time:&nbsp; {showLocalDateTime(endDateTime)}</li>
+        <li>Time left: </li>
+        {timerComponents.length ? timerComponents : <span>Time is up!</span>}
+      </ul>
+    </pre>
+  );
+}
